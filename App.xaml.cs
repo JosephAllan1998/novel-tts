0989a9769c5aa839dca64112e104004293686340
JosephAllan1998 <vumanhtuan1010@gmail.com>
2026-06-16 19:01:42 +0700

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+
+namespace novel_tts
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            LoadMissingDLL();
+        }
+
+        #region Events
+
+        private void LoadMissingDLL()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+            {
+                try
+                {
+                    var requestedAssembly = new AssemblyName(args.Name);
+                    string assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+                    string dllRequire = requestedAssembly.Name + ".dll";
+                    string dllPath = Path.Combine(assemblyPath, dllRequire);
+                    if (File.Exists(dllPath))
+                    {
+                        var _assembly = Assembly.LoadFrom(dllPath);
+                        return _assembly;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"DLL [{dllRequire}] not found in path [{assemblyPath}].");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error in AssemblyResolve handler.", ex);
+                }
+                return null;
+            };
+        }
+        #endregion Events
+    }
+}
