3d5cb86b5f158b905e140b430d4c53bd3e9f8818
JosephAllan1998 <vumanhtuan1010@gmail.com>
2026-06-16 22:42:10 +0700

    step05

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using novel_tts.Applications.Factories;
+using novel_tts.Applications.Services;
+using novel_tts.Core.Interfaces;
+using novel_tts.Infrastructure.Crawlers;
+using novel_tts.Infrastructure.Parsers;
+using novel_tts.Infrastructure.Repositories;
+using novel_tts.Infrastructure.Resilience;
+using novel_tts.Infrastructure.Services;
+using novel_tts.Infrastructure.TtsEngines;
+using novel_tts.Presentation.ViewModels;
+using System;
 using System.IO;
 using System.Reflection;
 using System.Windows;
@@ -10,9 +21,49 @@ namespace novel_tts
     /// </summary>
     public partial class App : Application
     {
+        public static IServiceProvider ServiceProvider { get; private set; }
+
         protected override void OnStartup(StartupEventArgs e)
         {
+            base.OnStartup(e);
+            
             LoadMissingDLL();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            // Hiển thị MainWindow và tiêm ViewModel vào
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+
+            // 1. Infrastructure (Singleton cho DB và Logger)
+            services.AddSingleton(new LoggerService(baseDir));
+            services.AddSingleton<PolicyProvider>();
+            services.AddSingleton<INovelRepository>(provider =>
+                new SqliteNovelRepository(baseDir, provider.GetRequiredService<LoggerService>()));
+
+            // 2. Engines & Http (Transient)
+            services.AddTransient<ICrawlerEngine, TruyenFullCrawler>();
+            services.AddTransient<IHtmlParser, TruyenFullParser>();
+
+            // Đăng ký các TTS Engines (Có thể thêm EdgeTtsEngine sau)
+            services.AddTransient<ITtsEngine, SystemSpeechTtsEngine>();
+            services.AddTransient<TtsEngineFactory>();
+
+            // 3. Application Services (Pipelines)
+            services.AddTransient<CrawlPipelineManager>();
+            services.AddTransient<MergePipelineManager>();
+            services.AddTransient<TtsPipelineManager>();
+
+            // 4. UI: ViewModels & Views
+            services.AddTransient<MainViewModel>();
+            services.AddTransient<MainWindow>();
         }
 
         #region Events
