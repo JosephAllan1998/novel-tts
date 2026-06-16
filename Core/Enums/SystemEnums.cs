a8319ff5a5571351cc0006dc594e116bf4f41299
JosephAllan1998 <vumanhtuan1010@gmail.com>
2026-06-16 20:10:07 +0700

    core stage

---
+++ Core/Enums/SystemEnums.cs
@@ -0,0 +1,18 @@
+﻿namespace novel_tts.Core.Enums
+{
+    public enum JobStatus
+    {
+        Pending = 0,
+        Processing = 1,
+        Completed = 2,
+        Failed = 3,
+        Paused = 4
+    }
+
+    public enum TtsEngineType
+    {
+        SystemSpeech = 1,
+        EdgeTts = 2,
+        GoogleTts = 3
+    }
+}
